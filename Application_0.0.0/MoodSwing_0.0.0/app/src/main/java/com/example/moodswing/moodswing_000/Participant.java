4bed9cdc0c32fa59140fa9cba287dd92af8f181c
BBest <bbest@ualberta.ca>
2017-02-21 15:11:25 -0700

    Completed the MoodEvent Testing and edited the image file names to not contain capitals

--- Application_0.0.0/MoodSwing_0.0.0/app/src/main/java/com/example/moodswing/moodswing_000/Participant.java
+++ Application_0.0.0/MoodSwing_0.0.0/app/src/main/java/com/example/moodswing/moodswing_000/Participant.java
@@ -1,5 +1,7 @@
 package com.example.moodswing.moodswing_000;
 
+import android.location.Location;
+
 import java.util.ArrayList;
 
 /**
@@ -14,6 +16,7 @@ public class Participant extends User {
 
 
     public Participant(String username){
+
         this.username = username;
     }
 
@@ -21,8 +24,8 @@ public class Participant extends User {
     // --- MoodEvent methods
 
     public void addMoodEvent(EmotionalState emotionalState, String trigger, SocialSituation socialSituation,
-                             String photoLocation, boolean saveLocation) {
-        moodHistory.add(new MoodEvent(emotionalState, trigger, socialSituation, photoLocation, saveLocation));
+                             String photoLocation, Location location) {
+        moodHistory.add(new MoodEvent(username ,emotionalState, trigger, socialSituation, photoLocation, location));
     }
 
     // --- end MoodEvent methods
