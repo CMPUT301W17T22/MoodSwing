ff327001153c955a47d30903ffcbab3086487f3f
fdrury <fdrury@ualberta.ca>
2017-02-25 10:28:04 -0700

    updated unit test shell properly

---
+++ Application_0.0.0/MoodSwing_0.0.0/app/src/test/java/com/example/moodswing/moodswing_000/MapsTest.java
@@ -0,0 +1,22 @@
+package com.example.moodswing.moodswing_000;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Fred on 2017-02-25.
+ */
+
+public class MapsTest {
+    //TODO: finish
+
+    /**
+     * create new MoodEvent with location information
+     * ensure that correct marker is returned
+     */
+    @Test
+    public void testMoodEventMarker(){
+
+    }
+}
