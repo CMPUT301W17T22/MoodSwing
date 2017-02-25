f97c73fbd4087c4941e51b24b927c486736d30f9
fdrury <fdrury@ualberta.ca>
2017-02-25 10:17:25 -0700

    added updated unit test shell

--- Application_0.0.0/MoodSwing_0.0.0/app/src/test/java/com/example/moodswing/moodswing_000/MapActivityTest.java
+++ Application_0.0.0/MoodSwing_0.0.0/app/src/test/java/com/example/moodswing/moodswing_000/MapActivityTest.java
@@ -2,6 +2,8 @@ package com.example.moodswing.moodswing_000;
 
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
 /**
  * Created by Fred on 2017-02-25.
  */
@@ -10,6 +12,6 @@ public class MapActivityTest {
     //TODO: finish
     @Test
     public void testAddBasicMarker(){
-        
+        assertTrue(false);
     }
 }
