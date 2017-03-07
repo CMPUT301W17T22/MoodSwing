a1a9436812fde22bcc232169208904a9fd2132e7
htruong1 <htruong1@ualberta.ca>
2017-03-07 08:34:57 -0700

    Java files for tabs
    Can't seem to resolve R issues. Not sure if I made a mistake or not.

---
+++ Application_0.0.0/MoodSwing_0.0.0/app/src/main/java/com/example/moodswing/moodswing_000/tab2FollowingList.java
@@ -0,0 +1,22 @@
+package com.example.moodswing.moodswing_000;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Henry on 2017-03-07.
+ */
+
+public class tab2FollowingList extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab2fFollowingFragment, container, false);
+
+        return rootView;
+    }
+}
