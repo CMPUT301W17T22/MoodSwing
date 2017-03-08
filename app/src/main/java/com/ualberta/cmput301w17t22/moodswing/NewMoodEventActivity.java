2fe6989f4d2361aee59823a2ac8c0368e597d1ff
Peter Weckend <pweckend@ualberta.ca>
2017-03-08 15:07:57 -0700

    New mood xml completed

--- app/src/main/java/com/ualberta/cmput301w17t22/moodswing/NewMoodEventActivity.java
+++ app/src/main/java/com/ualberta/cmput301w17t22/moodswing/NewMoodEventActivity.java
@@ -3,6 +3,12 @@ package com.ualberta.cmput301w17t22.moodswing;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * Activity that lets user add a new mood event to their mood history.
+ * Layout TODO:
+ * Increase spinner size
+ */
+
 public class NewMoodEventActivity extends AppCompatActivity {
 
     @Override
