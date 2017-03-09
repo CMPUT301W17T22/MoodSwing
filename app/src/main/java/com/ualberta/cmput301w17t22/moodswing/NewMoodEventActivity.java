cc67c1fc5f2ce0d5c63743c1e3e23999b2d1abbf
PeterWeckend <pweckend@ualberta.ca>
2017-03-09 15:21:20 -0700

    New string resources, spinner changes

--- app/src/main/java/com/ualberta/cmput301w17t22/moodswing/NewMoodEventActivity.java
+++ app/src/main/java/com/ualberta/cmput301w17t22/moodswing/NewMoodEventActivity.java
@@ -2,11 +2,12 @@ package com.ualberta.cmput301w17t22.moodswing;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Spinner;
 
 /**
  * Activity that lets user add a new mood event to their mood history.
  * Layout TODO:
- * Increase spinner size
+ * Increase spinner height
  */
 
 public class NewMoodEventActivity extends AppCompatActivity {
@@ -15,5 +16,11 @@ public class NewMoodEventActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_mood_event);
+
+        // going to just use pre-decided list of strings to represent moods and emotional states
+        // for now, its a lot easier than a dynamic list (like in MoodOptions) for now
+        // will have to change/update them if we end up changing the moods/states allowed
+        Spinner socialsituationSpinner = (Spinner) findViewById(R.id.socialSituationSpinner);
+        Spinner emotionalStateSpinner = (Spinner) findViewById(R.id.emotionalStateSpinner);
     }
 }
