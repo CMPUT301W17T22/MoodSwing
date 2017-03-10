09dda66d602565cf2d1a6db96707d8905528e658
PeterWeckend <pweckend@ualberta.ca>
2017-03-09 23:41:42 -0700

    NewMoodEvent trigger validation and error messages

--- app/src/main/java/com/ualberta/cmput301w17t22/moodswing/NewMoodEventActivity.java
+++ app/src/main/java/com/ualberta/cmput301w17t22/moodswing/NewMoodEventActivity.java
@@ -8,10 +8,13 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import java.util.Date;
 import java.util.Objects;
+import java.util.regex.Pattern;
 
 /**
  * Activity that lets user add a new mood event to their mood history.
@@ -20,10 +23,11 @@ import java.util.Objects;
  */
 
 public class NewMoodEventActivity extends AppCompatActivity {
+    // need to add this view to moodswing application?
+
     // for camera
     static final int REQUEST_IMAGE_CAPTURE = 1;
 
-    // need to add this view to moodswing application?
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,6 +42,9 @@ public class NewMoodEventActivity extends AppCompatActivity {
         final Button newMoodEventPostButton = (Button) findViewById(R.id.newMoodEventPostButton);
         Button photoUploadButton = (Button) findViewById(R.id.photoUploadButton);
 
+        // use current date/time for MoodEvent
+        Date moodDate = new Date();
+
         // photo upload press. Need a real device to test this on
         // https://developer.android.com/training/camera/photobasics.html
         photoUploadButton.setOnClickListener(new View.OnClickListener(){
@@ -48,21 +55,28 @@ public class NewMoodEventActivity extends AppCompatActivity {
             }
         });
 
-        // Post button press. At the moment, just closes activity
+
+        // Occurs when you press "Post" button. At the moment, just closes activity
         newMoodEventPostButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                //validtriggers(v);
                 if (Objects.equals(String.valueOf(emotionalStateSpinner.getSelectedItem()),"")){
                     Toast.makeText(NewMoodEventActivity.this,
                             "Please enter an emotional state.",
+                            Toast.LENGTH_SHORT).show();
+                }
+                else if (!validtriggers(v)){
+                    Toast.makeText(NewMoodEventActivity.this,
+                            "Trigger length too long.",
                             Toast.LENGTH_SHORT).show();
                 }
                 else {
                     Toast.makeText(NewMoodEventActivity.this,
                             "Input Test: " +
                                     "\nEmotional State: "+ String.valueOf(emotionalStateSpinner.getSelectedItem()) +
-                                    "\nSocial Situation: "+ String.valueOf(socialsituationSpinner.getSelectedItem()),
+                                    "\nSocial Situation: "+ String.valueOf(socialsituationSpinner.getSelectedItem()) +
+                            String.valueOf(validtriggers(v)),
                             Toast.LENGTH_SHORT).show();
                     finish();
                 }
@@ -75,6 +89,7 @@ public class NewMoodEventActivity extends AppCompatActivity {
 
     // for taking a picture
     // https://github.com/DroidNinja/Android-FilePicker
+    // https://developer.android.com/training/camera/photobasics.html
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
@@ -82,13 +97,33 @@ public class NewMoodEventActivity extends AppCompatActivity {
         }
     }
 
-    // return 1 if addCurentLocationCheckBox is checked, 0 otherwise
+    // return true if addCurentLocationCheckBox is checked, false otherwise
     public boolean includeLocationCheck(View v){
         CheckBox addCurentLocationCheckBox = (CheckBox) findViewById(R.id.addCurentLocationCheckBox);
         //            Toast.makeText(NewMoodEventActivity.this,
 //                    "Checked", Toast.LENGTH_LONG).show();
         return addCurentLocationCheckBox.isChecked();
     }
+
+    // return true if entered triggers are 3 words or < 20 chars, false otherwise
+    public boolean validtriggers(View v){
+        EditText triggerEditText = (EditText)findViewById(R.id.triggerEditText);
+        String triggers = triggerEditText.getText().toString();
+
+        int trigger_charlen = triggers.length();
+        String[] trigger_split = triggers.split(Pattern.quote(" "));
+        int trigger_split_len = trigger_split.length;
+
+        // test output
+//        Toast.makeText(NewMoodEventActivity.this,
+//                Integer.toString(trigger_split_len), Toast.LENGTH_SHORT).show();
+
+        if (trigger_split_len <= 3 || trigger_charlen < 20){
+            return true;
+        } else {
+            return false;
+        }}
+
 
 
 
