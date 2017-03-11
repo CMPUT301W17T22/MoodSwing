7348c4eeafb0b5b99e200a3c54cf2dd455f321be
fdrury <fdrury@ualberta.ca>
2017-03-10 22:18:54 -0700

    updated map unit test

--- app/src/test/java/com/ualberta/cmput301w17t22/moodswing/MapsTest.java
+++ app/src/test/java/com/ualberta/cmput301w17t22/moodswing/MapsTest.java
@@ -2,7 +2,11 @@ package com.ualberta.cmput301w17t22.moodswing;
 
 import android.graphics.Color;
 
+import com.google.android.gms.maps.model.LatLng;
+
 import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
 
 /**
  * Created by Fred on 2017-02-25.
@@ -19,6 +23,13 @@ public class MapsTest {
     public void testMoodEventMarker(){
         String trigger = "trigger";
         EmotionalState emotionalState = new EmotionalState("happy","somewhere", Color.BLACK);
-        MoodEvent moodEvent = new MoodEvent("username1",emotionalState, trigger, null, null, null, null);
+        double latitude = -34;
+        double longitude = 151;
+        LatLng location = new LatLng(latitude, longitude); //Sydney
+        MoodEvent moodEvent = new MoodEvent("username1",emotionalState, trigger, null, null, null, location);
+
+        LatLng moodEventLocation = moodEvent.getLocation();
+        assertEquals(moodEventLocation.latitude, latitude);
+        assertEquals(moodEventLocation.longitude, longitude);
     }
 }
