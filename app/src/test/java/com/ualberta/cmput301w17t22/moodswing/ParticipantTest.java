d4cc917c0b1f8fe6360475851740cb7a296e38b1
PeterWeckend <pweckend@ualberta.ca>
2017-03-13 15:01:41 -0600

    All participant unit tests done

---
+++ app/src/test/java/com/ualberta/cmput301w17t22/moodswing/ParticipantTest.java
@@ -0,0 +1,178 @@
+package com.ualberta.cmput301w17t22.moodswing;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import org.junit.Test;
+
+import java.util.Date;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by PeterWeckend on 2017-03-13.
+ * Tests out Participant class.
+ * Other Participant methods tested by Follower/Following Unit Tests.
+ */
+
+public class ParticipantTest {
+
+    /**
+     * Testing out adding a mood event.
+     */
+    @Test
+    public void testAddMoodEvent() {
+        // initializing
+        Participant participant1 = new Participant("participant1");
+        Date date = new Date();
+        EmotionalState emotionalState = new EmotionalState("my emo state");
+        String trigger = "my trigger";
+        SocialSituation socialSituation = new SocialSituation("my soc state");
+        LatLng location = new LatLng(0,0);
+
+        assertTrue(participant1.getMoodHistory().isEmpty());    // moodhistory empty
+        // add mood event
+        participant1.addMoodEvent(date, emotionalState, trigger, socialSituation, location);
+        // mood event added successfully
+        assertTrue(participant1.getMoodHistory().get(0).getTrigger().equals(trigger));
+        assertTrue(participant1.getMoodHistory().get(0).getOriginalPoster().equals("participant1"));
+        assertTrue(participant1.getMoodHistory().get(0).getDate().equals(date));
+        assertTrue(participant1.getMoodHistory().get(0).getEmotionalState().equals(emotionalState));
+        assertTrue(participant1.getMoodHistory().get(0).getLocation().equals(location));
+
+        // test negatives
+        assertFalse(participant1.getMoodHistory().get(0).getOriginalPoster().equals("participant2"));
+
+    }
+
+
+    /**
+     * Testing out editing a mood event at a certain position.
+     */
+    @Test
+    public void testEditMoodEventByPosition() {
+        // initializing
+        Participant participant1 = new Participant("participant1");
+        Date date = new Date();
+        EmotionalState emotionalState = new EmotionalState("my emo state");
+        String trigger = "my trigger";
+        SocialSituation socialSituation = new SocialSituation("my soc state");
+        LatLng location = new LatLng(0,0);
+
+        Date date2 = new Date();
+        EmotionalState emotionalState2 = new EmotionalState("my emo state2");
+        String trigger2 = "my trigger2";
+        SocialSituation socialSituation2 = new SocialSituation("my soc state2");
+        LatLng location2 = new LatLng(0,1);
+
+        Date date3 = new Date();
+        EmotionalState emotionalState3 = new EmotionalState("my emo state3");
+        String trigger3 = "my trigger3";
+        SocialSituation socialSituation3 = new SocialSituation("my soc state3");
+        LatLng location3 = new LatLng(0,2);
+
+        assertTrue(participant1.getMoodHistory().isEmpty());    // moodhistory empty
+        // add mood event
+        participant1.addMoodEvent(date, emotionalState, trigger, socialSituation, location);
+        participant1.addMoodEvent(date2, emotionalState2, trigger2, socialSituation2, location2);
+
+        // edit first mood event
+        participant1.editMoodEventByPosition(0,date3, emotionalState3, trigger3, socialSituation3, location3);
+
+
+        // check first event is edited
+        assertTrue(participant1.getMoodHistory().get(0).getTrigger().equals(trigger3));
+        assertTrue(participant1.getMoodHistory().get(0).getOriginalPoster().equals("participant1"));
+        assertTrue(participant1.getMoodHistory().get(0).getDate().equals(date3));
+        assertTrue(participant1.getMoodHistory().get(0).getEmotionalState().equals(emotionalState3));
+        assertTrue(participant1.getMoodHistory().get(0).getLocation().equals(location3));
+
+
+        // check second event still there and hasn't changed
+        assertTrue(participant1.getMoodHistory().get(1).getTrigger().equals(trigger2));
+        assertTrue(participant1.getMoodHistory().get(1).getOriginalPoster().equals("participant1"));
+        assertTrue(participant1.getMoodHistory().get(1).getDate().equals(date2));
+        assertTrue(participant1.getMoodHistory().get(1).getEmotionalState().equals(emotionalState2));
+
+    }
+
+    /**
+     * Testing out deleting a mood event at a certain position.
+     */
+    @Test
+    public void testDeleteMoodEventByPosition() {
+        // initializing
+        Participant participant1 = new Participant("participant1");
+        Date date = new Date();
+        EmotionalState emotionalState = new EmotionalState("my emo state");
+        String trigger = "my trigger";
+        SocialSituation socialSituation = new SocialSituation("my soc state");
+        LatLng location = new LatLng(0,0);
+
+        Date date2 = new Date();
+        EmotionalState emotionalState2 = new EmotionalState("my emo state2");
+        String trigger2 = "my trigger2";
+        SocialSituation socialSituation2 = new SocialSituation("my soc state2");
+        LatLng location2 = new LatLng(0,1);
+
+        assertTrue(participant1.getMoodHistory().isEmpty());    // moodhistory empty
+        // add mood event
+        participant1.addMoodEvent(date, emotionalState, trigger, socialSituation, location);
+        participant1.addMoodEvent(date2, emotionalState2, trigger2, socialSituation2, location2);
+
+        // delete first mood event
+        participant1.deleteMoodEventByPosition(0);
+        // check second event still there in first event's position
+        assertTrue(participant1.getMoodHistory().get(0).getTrigger().equals(trigger2));
+        assertTrue(participant1.getMoodHistory().get(0).getOriginalPoster().equals("participant1"));
+        assertTrue(participant1.getMoodHistory().get(0).getDate().equals(date2));
+        assertTrue(participant1.getMoodHistory().get(0).getEmotionalState().equals(emotionalState2));
+        assertTrue(participant1.getMoodHistory().get(0).getLocation().equals(location2));
+
+        // delete second mood event
+        participant1.deleteMoodEventByPosition(0);
+        assertTrue(participant1.getMoodHistory().isEmpty());    // moodhistory empty
+    }
+
+
+    /**
+     * Testing out getting the most recent mood event.
+     */
+    @Test
+    public void testGetMostRecentMoodEvent() {
+        // initializing
+        Participant participant1 = new Participant("participant1");
+        Date date = new Date();
+        EmotionalState emotionalState = new EmotionalState("my emo state");
+        String trigger = "my trigger";
+        SocialSituation socialSituation = new SocialSituation("my soc state");
+        LatLng location = new LatLng(0,0);
+
+        Date date2 = new Date();
+        EmotionalState emotionalState2 = new EmotionalState("my emo state2");
+        String trigger2 = "my trigger2";
+        SocialSituation socialSituation2 = new SocialSituation("my soc state2");
+        LatLng location2 = new LatLng(0,1);
+
+        assertTrue(participant1.getMoodHistory().isEmpty());    // moodhistory empty
+        // add mood event
+        participant1.addMoodEvent(date, emotionalState, trigger, socialSituation, location);
+        participant1.addMoodEvent(date2, emotionalState2, trigger2, socialSituation2, location2);
+
+        // getmostrecentmoodevent grabs mood event #2
+        assertTrue(participant1.getMostRecentMoodEvent().getTrigger().equals(trigger2));
+        assertTrue(participant1.getMostRecentMoodEvent().getOriginalPoster().equals("participant1"));
+        assertTrue(participant1.getMostRecentMoodEvent().getDate().equals(date2));
+        assertTrue(participant1.getMostRecentMoodEvent().getEmotionalState().equals(emotionalState2));
+        assertTrue(participant1.getMostRecentMoodEvent().getLocation().equals(location2));
+
+        // check it doesn't grab #1
+        assertFalse(participant1.getMostRecentMoodEvent().getTrigger().equals(trigger));
+        assertFalse(participant1.getMostRecentMoodEvent().getEmotionalState().equals(emotionalState));
+        assertFalse(participant1.getMostRecentMoodEvent().getLocation().equals(location));
+
+    }
+
+
+}
