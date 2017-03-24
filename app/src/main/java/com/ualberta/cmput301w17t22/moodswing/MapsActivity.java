d3d7d49cdcec01d858288799d3b16b1517774255
htruong1 <htruong1@ualberta.ca>
2017-03-23 19:38:20 -0600

    partially implemented easy location stuff

--- app/src/main/java/com/ualberta/cmput301w17t22/moodswing/MapsActivity.java
+++ app/src/main/java/com/ualberta/cmput301w17t22/moodswing/MapsActivity.java
@@ -1,5 +1,6 @@
 package com.ualberta.cmput301w17t22.moodswing;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.support.v4.app.ActivityCompat;
@@ -38,30 +39,39 @@ public class MapsActivity extends FragmentActivity implements LocationListener, 
         mapFragment.getMapAsync(this);
 
         //get location service
-        lm=(LocationManager)this.getSystemService(Context.LOCATION_SERVICE);
-        Criteria c=new Criteria();
+        lm = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        Criteria c = new Criteria();
         //criteria object will select best service based on
         //Accuracy, power consumption, response, bearing and monetary cost
         //set false to use best service otherwise it will select the default Sim network
         //and give the location based on sim network
         //now it will first check satellite than Internet than Sim network location
-        provider=lm.getBestProvider(c, false);
+        provider = lm.getBestProvider(c, false);
         //now you have best provider
         //get location
         // http://stackoverflow.com/questions/32491960/android-check-permission-for-locationmanager
-        if ( ContextCompat.checkSelfPermission( this, android.Manifest.permission.ACCESS_COARSE_LOCATION ) != PackageManager.PERMISSION_GRANTED ) {
+        /*if ( ContextCompat.checkSelfPermission( this, android.Manifest.permission.ACCESS_COARSE_LOCATION ) != PackageManager.PERMISSION_GRANTED ) {
             //TODO: actually ask permission
             //TODO: fix '8'
             ActivityCompat.requestPermissions( this, new String[] {  android.Manifest.permission.ACCESS_COARSE_LOCATION  },
                     8 );
+        }*/
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
         }
-        l=lm.getLastKnownLocation(provider);
+        l = lm.getLastKnownLocation(provider);
         if(l!=null)
         {
             //get latitude and longitude of the location
             double lng=l.getLongitude();
             double lat=l.getLatitude();
-            //display on text view
             //TODO: update map based on location
             mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(lat, lng)));
         }
