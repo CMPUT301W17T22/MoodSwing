016876f04737c8a4f39dd63faa369b9f08c84ea6
htruong1 <htruong1@ualberta.ca>
2017-03-30 17:49:31 -0600

    MapVie for  moodFeed is no longer continental

--- app/src/main/java/com/ualberta/cmput301w17t22/moodswing/MainActivity.java
+++ app/src/main/java/com/ualberta/cmput301w17t22/moodswing/MainActivity.java
@@ -132,6 +132,11 @@ public class MainActivity extends AppCompatActivity implements MSView<MoodSwing>
      * Called on opening of activity for the first time.
      * @param savedInstanceState
      */
+
+    //command function that executes
+    public interface Command{
+        public void execute();
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -442,6 +447,7 @@ public class MainActivity extends AppCompatActivity implements MSView<MoodSwing>
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mMap.setMinZoomPreference(10.0f); //minimum zoom set to
         mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(lastKnownLat, lastKnownLng)));
     }
 
