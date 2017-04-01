66691e3f9f1017a68f9622e4099855b17c8c5e8b
fdrury <fdrury@ualberta.ca>
2017-04-01 15:33:34 -0600

    added network change receiver but need to register it (from context)

---
+++ app/src/main/java/com/ualberta/cmput301w17t22/moodswing/NetworkStateReceiver.java
@@ -0,0 +1,34 @@
+package com.ualberta.cmput301w17t22.moodswing;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.util.Log;
+
+/**
+ * http://stackoverflow.com/questions/3767591/check-intent-internet-connection
+ * grab date 01-04-2017
+ */
+
+public class NetworkStateReceiver extends BroadcastReceiver {
+    private static final String TAG = "NetworkStateReceiver";
+
+    @Override
+    public void onReceive(final Context context, final Intent intent) {
+
+        Log.d(TAG, "Network connectivity change");
+
+        if (intent.getExtras() != null) {
+            final ConnectivityManager connectivityManager = (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+            final NetworkInfo ni = connectivityManager.getActiveNetworkInfo();
+
+            if (ni != null && ni.isConnectedOrConnecting()) {
+                Log.i(TAG, "Network " + ni.getTypeName() + " connected");
+            } else if (intent.getBooleanExtra(ConnectivityManager.EXTRA_NO_CONNECTIVITY, Boolean.FALSE)) {
+                Log.d(TAG, "There's no network connectivity");
+            }
+        }
+    }
+}
